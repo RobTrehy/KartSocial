a5b4c124a732106a628f5017f604067d3f141fa9
Robert Trehy <robert.trehy@outlook.com>
2023-11-14 15:40:06 +0000

    Dashboard Controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DashboardFeed;
+use App\Models\Track;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display the dashboard feed
+     * 
+     * Requires: dashboard.view
+     */
+    public function feed()
+    {
+        if (Auth::user()->cannot('dashboard.view')) {
+            abort(403);
+        }
+
+        return Inertia::render('Dashboard/Dashboard', [
+            'feed' => DashboardFeed::whereIn(
+                'user_id',
+                User::find(Auth::id())
+                    ->follows()
+                    ->pluck('id')
+            )->orderBy('created_at', 'DESC')->get(),
+            'suggestions' => User::find(Auth::id())
+                ->load(['suggestions' => function ($query) {
+                    $query->inRandomOrder()->take('5');
+                }, 'suggestions.suggested', 'suggestions.via'])->suggestions,
+            'tracks' => Track::whereNotIn(
+                'id',
+                User::find(Auth::id())
+                    ->load('trackVisits')
+                    ->pluck('track_visits.track_layout.track.id')
+            )->inRandomOrder()->take(5)->get()
+        ]);
+    }
+}
