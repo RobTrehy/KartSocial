da1009fd2f507598b15db10863364b4df8593d91
Robert Trehy <robert.trehy@outlook.com>
2023-11-14 13:08:24 +0000

    Add brand colours to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,13 +12,33 @@ export default {
         './resources/js/**/*.tsx',
     ],
 
+    darkMode: 'class',
+
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'brand': {
+                    '50': '#f2f6fc',
+                    '100': '#e2ecf7',
+                    '200': '#ccddf1',
+                    '300': '#a9c8e7',
+                    '400': '#80abda',
+                    '500': '#628fcf',
+                    '600': '#4e76c2',
+                    '700': '#4464b1',
+                    '800': '#3c5391',
+                    '900': '#344774',
+                    '950': '#2d395a',
+                },
+            }
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [
+        forms,
+        typography
+    ],
 };
