f943eb5ac40bbf1cc2154f54c80748d6e483503d
Robert Trehy <robert.trehy@outlook.com>
2023-11-14 11:06:46 +0000

    Add cost and colors to roles

--- database/migrations/2023_11_14_110334_create_permission_tables.php
+++ database/migrations/2023_11_14_110334_create_permission_tables.php
@@ -40,6 +40,8 @@ return new class extends Migration
                 $table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
             }
             $table->string('name');       // For MySQL 8.0 use string('name', 125);
+            $table->decimal('cost', 8, 2)->nullable();
+            $table->json('colors');
             $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
             $table->timestamps();
             if ($teams || config('permission.testing')) {
