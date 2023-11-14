2764ea5290b2988a2e033d6d6a01d98145bf0ba6
Robert Trehy <robert.trehy@outlook.com>
2023-11-14 13:09:27 +0000

    Add branding

--- resources/js/Components/PrimaryButton.tsx
+++ resources/js/Components/PrimaryButton.tsx
@@ -14,7 +14,7 @@ export default function PrimaryButton({
     <button
       {...props}
       className={classNames(
-        'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150',
+        'inline-flex items-center px-4 py-2 bg-brand-800 dark:bg-brand-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-brand-700 dark:hover:bg-white focus:bg-brand-700 dark:focus:bg-white active:bg-brand-900 dark:active:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:focus:ring-offset-brand-800 transition ease-in-out duration-150',
         props.className,
       )}
     >
