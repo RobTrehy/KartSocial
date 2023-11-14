2e13f600f786bc6cd97fcbf7a83347d85a5e49a1
Robert Trehy <robert.trehy@outlook.com>
2023-11-14 14:28:21 +0000

    Update Fortify Actions

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,9 +2,12 @@
 
 namespace App\Actions\Fortify;
 
+use App\Models\Invitation;
 use App\Models\User;
+use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
 
@@ -21,13 +24,29 @@ class CreateNewUser implements CreatesNewUsers
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
+            'alias' => [
+                'required',
+                Rule::unique('users', 'alias'),
+                'alpha_dash:ascii',
+            ],
+            'dob' => ['required', 'date', 'before:-13 years'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ], [
+            'dob.before' => 'You must be over 13 years of age to use this service.'
         ])->validate();
+
+        if (Env::get('APP_INVITATION_ONLY', false)) {
+            $invitation = Invitation::where('email', $input['email'])->first();
+            $invitation->registered_at = now();
+            $invitation->save();
+        }
 
         return User::create([
             'name' => $input['name'],
+            'alias' => $input['alias'],
+            'dob' => $input['dob'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
