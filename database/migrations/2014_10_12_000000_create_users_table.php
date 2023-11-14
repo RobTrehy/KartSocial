7a332db8ce61de70e183d315b3f37b8cd8a1f660
Robert Trehy <robert.trehy@outlook.com>
2023-11-14 10:59:48 +0000

    Add profile fields to users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('alias');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('dob');
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('cover_photo_path', 2048)->nullable();
+            $table->string('bio', 120)->nullable();
+            $table->decimal('weight', 10, 1)->nullable();
             $table->timestamps();
         });
     }
