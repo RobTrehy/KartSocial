4586a351fa608c5185cde773d9a182b318983350
Robert Trehy <robert.trehy@outlook.com>
2023-11-14 15:56:32 +0000

    Hooks

---
+++ resources/js/Hooks/usePermissions.ts
@@ -0,0 +1,7 @@
+import { InertiaSharedProps } from '@/types';
+import { usePage } from '@inertiajs/react';
+
+export default function usePermissions<T = {}>() {
+  const page = usePage<InertiaSharedProps<T>>();
+  return (page.props.auth.permissions) ? page.props.auth.permissions : [];
+}
