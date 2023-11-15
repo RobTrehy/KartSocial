6aba1bfd933ceeb8b36b914a6f5e98f426f891f5
Robert Trehy <robert.trehy@outlook.com>
2023-11-15 12:01:27 +0000

    Add home track to users

---
+++ database/migrations/2023_11_15_120055_add_home_track_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('home_track_id')->after('cover_photo_path')->nullable()->constrained('tracks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->removeColumn('home_track_id');
+        });
+    }
+};
