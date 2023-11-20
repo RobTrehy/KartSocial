8a6733ce3a01f3aaf837a11aec126bb0368826a3
Robert Trehy <robert.trehy@outlook.com>
2023-11-20 15:02:08 +0000

    Remove links from Logos

--- resources/js/Components/Logos/InitialsLogo.tsx
+++ resources/js/Components/Logos/InitialsLogo.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@inertiajs/react';
 import classNames from 'classnames';
 import React from 'react';
 
@@ -9,8 +8,6 @@ interface Props {
 
 export default function IntialsLogo({ classes, secondaryClasses }: Props) {
   return (
-    <Link href="/">
-      <h1 className={classNames("brand-font", classes)}>K<span className={secondaryClasses}>S</span></h1>
-    </Link>
+    <h1 className={classNames("brand-font", classes)}>K<span className={secondaryClasses}>S</span></h1>
   );
 }
--- resources/js/Components/Logos/TextLogo.tsx
+++ resources/js/Components/Logos/TextLogo.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@inertiajs/react';
 import classNames from 'classnames';
 import React from 'react';
 
@@ -9,8 +8,6 @@ interface Props {
 
 export default function TextLogo({ classes, secondaryClasses }: Props) {
   return (
-    <Link href="/">
-      <h1 className={classNames("brand-font", classes)}>Kart <span className={secondaryClasses}>Social</span></h1>
-    </Link>
+    <h1 className={classNames("brand-font", classes)}>Kart <span className={secondaryClasses}>Social</span></h1>
   );
 }
