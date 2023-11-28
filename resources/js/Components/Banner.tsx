46b40bfa0ca7aba236df50ab005d4e5b70329ca5
Robert Trehy <robert.trehy@outlook.com>
2023-11-28 14:42:24 +0000

    Use brand colours!

--- resources/js/Components/Banner.tsx
+++ resources/js/Components/Banner.tsx
@@ -13,7 +13,7 @@ export default function Banner() {
       {show && message ? (
         <div
           className={classNames({
-            'bg-indigo-500': style == 'success',
+            'bg-brand-500': style == 'success',
             'bg-red-700': style == 'danger',
           })}
         >
@@ -22,7 +22,7 @@ export default function Banner() {
               <div className="w-0 flex-1 flex items-center min-w-0">
                 <span
                   className={classNames('flex p-2 rounded-md', {
-                    'bg-indigo-600': style == 'success',
+                    'bg-brand-600': style == 'success',
                     'bg-red-600': style == 'danger',
                   })}
                 >
@@ -79,7 +79,7 @@ export default function Banner() {
                   className={classNames(
                     '-mr-1 flex p-2 rounded-md focus:outline-none md:-mr-2 transition',
                     {
-                      'hover:bg-indigo-600 focus:bg-indigo-600':
+                      'hover:bg-brand-600 focus:bg-brand-600':
                         style == 'success',
                       'hover:bg-red-600 focus:bg-red-600': style == 'danger',
                     },
