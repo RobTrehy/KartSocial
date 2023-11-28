40c14dd63f59a4fbdf00c050071bfdaf696bb536
Robert Trehy <robert.trehy@outlook.com>
2023-11-28 13:30:26 +0000

    Update Email Verification email

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +22,16 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Confirm your email address')
+                ->greeting('Verify Your Email Account')
+                ->line('You\'re registered to use Kart Social!')
+                ->line('In order complete the sign up process, please click the button below to verify your email address.')
+                ->action('Confirm email address', $url)
+                ->line('If you didn\'t sign up for Kart Social, you can safely ignore this email.')
+                ->line('Thanks,')
+                ->salutation('The Kart Social Team');
+        });
     }
 }
