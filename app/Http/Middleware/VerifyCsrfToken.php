b859b518fcd9618d83a3ecb9f862ba4c6e8d3373
Robert Trehy <robert.trehy@outlook.com>
2023-12-20 10:17:31 +0000

    Don't require CSRF for notification routes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'push',
+        'api/notifications/*/read',
     ];
 }
