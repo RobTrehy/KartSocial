3b59444da48df1d47706f83bc4805e9c9f98b2ff
Robert Trehy <robert.trehy@outlook.com>
2024-01-08 11:39:16 +0000

    Show import ready when file selected and processed

--- resources/js/Pages/Track/Laps/Partials/AlphaDataImporter.tsx
+++ resources/js/Pages/Track/Laps/Partials/AlphaDataImporter.tsx
@@ -10,7 +10,7 @@ export default function AlphaDataImporter({ }: any) {
 
   const fileReader = new FileReader();
 
-  const { importLaps, setImportLaps, setImportReady }: any =
+  const { importLaps, setImportLaps, importReady, setImportReady }: any =
     useContext(sessionLapContext);
 
   useEffect(() => {
@@ -55,40 +55,53 @@ export default function AlphaDataImporter({ }: any) {
 
   return (
     <div className="flex flex-wrap gap-2 mt-2">
-      {!excelRows && (
-        <label className="cursor-pointer items-center text-center w-full border-2 rounded-md border-dashed hover:bg-gray-100 hover:border-gray-300">
-          <div className="flex flex-row items-center justify-center pt-3">
-            <p className="text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
-              Upload File
-            </p>
+      {
+        !importReady ? (
+          <>
+            {!excelRows && (
+              <label className="cursor-pointer items-center text-center w-full border-2 rounded-md border-dashed hover:bg-gray-100 hover:border-gray-300">
+                <div className="flex flex-row items-center justify-center pt-3">
+                  <p className="text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                    Upload File
+                  </p>
+                </div>
+                <input
+                  type="file"
+                  onChange={handleOnSubmit}
+                  accept={'.xlsx'}
+                  className="opacity-0"
+                />
+              </label>
+            )}
+            {excelRows && (
+              <Select
+                id="driver"
+                name="driver"
+                title="Driver"
+                onChange={e => setDriver(e.currentTarget.value)}
+              >
+                <option>Select Driver...</option>
+                {Object.keys(excelRows[1]).map(row => {
+                  if (row.startsWith('__EMPTY')) {
+                    return (
+                      <option value={row} key={row}>
+                        {excelRows[1][row]}
+                      </option>
+                    );
+                  }
+                })}
+              </Select>
+            )}
+          </>
+        ) : (
+          <div className="flex flex-row dark:text-gray-400 items-center gap-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 stroke-lime-600">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <p>Ready for import!</p>
           </div>
-          <input
-            type="file"
-            onChange={handleOnSubmit}
-            accept={'.xlsx'}
-            className="opacity-0"
-          />
-        </label>
-      )}
-      {excelRows && (
-        <Select
-          id="driver"
-          name="driver"
-          title="Driver"
-          onChange={e => setDriver(e.currentTarget.value)}
-        >
-          <option>Select Driver...</option>
-          {Object.keys(excelRows[1]).map(row => {
-            if (row.startsWith('__EMPTY')) {
-              return (
-                <option value={row} key={row}>
-                  {excelRows[1][row]}
-                </option>
-              );
-            }
-          })}
-        </Select>
-      )}
+        )
+      }
     </div>
   );
 }
