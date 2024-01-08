ab68a37d5efd72a188cdbece1d1675eff460305c
Robert Trehy <robert.trehy@outlook.com>
2024-01-08 11:38:42 +0000

    Improve layout and dark mode

--- resources/js/Pages/Track/Laps/Partials/ImportDataModal.tsx
+++ resources/js/Pages/Track/Laps/Partials/ImportDataModal.tsx
@@ -64,13 +64,13 @@ export default function ImportDataModal({ open, setOpen, event }: any) {
               leaveFrom="opacity-100 translate-y-0 md:scale-100"
               leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-md bg-white text-left shadow-xl transition-all w-full max-w-full md:my-8 md:w-full md:max-w-3xl">
-                <div className="bg-white px-4 pb-4 pt-5 md:p-6 md:pb-4">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-md bg-white dark:bg-gray-900 text-left shadow-xl transition-all w-full max-w-full md:my-8 md:w-full md:max-w-4xl">
+                <div className="px-4 pb-4 pt-5 md:p-6 md:pb-4">
                   <div className="md:flex md:items-start">
                     <div className="mt-3 text-center md:ml-4 md:mt-0 md:text-left">
                       <Dialog.Title
                         as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
+                        className="text-base font-semibold leading-6 text-gray-900 dark:text-white"
                       >
                         Import Data
                       </Dialog.Title>
@@ -81,23 +81,23 @@ export default function ImportDataModal({ open, setOpen, event }: any) {
                               htmlFor=""
                               value="Select the type of data you wish to import"
                             />
-                            <ul className="mt-1 grid w-full gap-6 md:grid-cols-2">
+                            <ul className="mt-1 grid w-full gap-4 md:grid-cols-3">
                               <li>
                                 <input
                                   type="radio"
-                                  id="csv-data"
+                                  id="alpha-data"
                                   name="data-type"
-                                  value="csv-data"
+                                  value="alpha-data"
                                   className="hidden peer"
                                   required
                                   onChange={() => setDataType('alpha-xlsx')}
                                 />
                                 <label
-                                  htmlFor="csv-data"
-                                  className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-md cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                                  htmlFor="alpha-data"
+                                  className="inline-flex items-center justify-between w-full p-6 text-gray-500 bg-white border border-gray-200 rounded-md cursor-pointer dark:border-gray-700 dark:hover:text-brand-500 hover:border-brand-600 dark:hover:border-brand-500 hover:text-brand-600 hover:bg-brand-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-brand-900"
                                 >
                                   <div className="block">
-                                    <div className="w-full text-lg font-semibold">
+                                    <div className="w-full text-md font-semibold">
                                       Alpha Timing XLSX File
                                     </div>
                                   </div>
@@ -129,10 +129,10 @@ export default function ImportDataModal({ open, setOpen, event }: any) {
                                 />
                                 <label
                                   htmlFor="app-screens"
-                                  className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-md cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                                  className="inline-flex items-center justify-between w-full p-6 text-gray-500 bg-white border border-gray-200 rounded-md cursor-pointer dark:border-gray-700 dark:hover:text-brand-500 hover:border-brand-600 dark:hover:border-brand-500 hover:text-brand-600 hover:bg-brand-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-brand-900"
                                 >
                                   <div className="block">
-                                    <div className="w-full text-lg font-semibold">
+                                    <div className="w-full text-md font-semibold">
                                       App Screenshots
                                     </div>
                                   </div>
@@ -156,19 +156,19 @@ export default function ImportDataModal({ open, setOpen, event }: any) {
                               <li>
                                 <input
                                   type="radio"
-                                  id="csv-data"
+                                  id="racechrono-data"
                                   name="data-type"
-                                  value="csv-data"
+                                  value="racechrono-data"
                                   className="hidden peer"
                                   required
                                   onChange={() => setDataType('racechrono')}
                                 />
                                 <label
-                                  htmlFor="csv-data"
-                                  className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-md cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                                  htmlFor="racechrono-data"
+                                  className="inline-flex items-center justify-between w-full p-6 text-gray-500 bg-white border border-gray-200 rounded-md cursor-pointer dark:border-gray-700 dark:hover:text-brand-500 hover:border-brand-600 dark:hover:border-brand-500 hover:text-brand-600 hover:bg-brand-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-brand-900"
                                 >
                                   <div className="block">
-                                    <div className="w-full text-lg font-semibold">
+                                    <div className="w-full text-md font-semibold">
                                       Race Chrono ODS File
                                     </div>
                                   </div>
@@ -192,42 +192,36 @@ export default function ImportDataModal({ open, setOpen, event }: any) {
                             </ul>
                           </>
                         )}
-                        {dataType === 'alpha-xlsx' && (
-                          <AlphaDataImporter setProcessing={setProcessing} />
-                        )}
-                        {dataType === 'app-screens' && (
-                          <AppScreenImporter setProcessing={setProcessing} />
-                        )}
-                        {dataType === 'racechrono' && (
-                          <RaceChronoDataImporter setProcessing={setProcessing} />
-                        )}
+                        {dataType === 'alpha-xlsx' && <AlphaDataImporter setProcessing={setProcessing} />}
+                        {dataType === 'app-screens' && <AppScreenImporter setProcessing={setProcessing} />}
+                        {dataType === 'racechrono' && <RaceChronoDataImporter setProcessing={setProcessing} />}
                       </div>
-                      <div className="mt-2">
-                        <InputLabel htmlFor="import_session" value="Session to import to" />
-                        <Select
-                          id="import_session"
-                          name="import_session"
-                          value={importSession}
-                          onChange={e => setImportSession(e.currentTarget.value)}
-                        >
-                          {event.sessions.length > 0 && event.sessions.map((session: TrackSession, i: number) => (
-                            <option key={i} value={session.id}>{session.name}</option>
-                          ))}
-                        </Select>
-                      </div>
+                      {
+                        importReady && (
+                          <div className="mt-2">
+                            <InputLabel htmlFor="import_session" value="Session to import to" />
+                            <Select
+                              id="import_session"
+                              name="import_session"
+                              value={importSession}
+                              onChange={e => setImportSession(e.currentTarget.value)}
+                            >
+                              {event.sessions.length > 0 && event.sessions.map((session: TrackSession, i: number) => (
+                                <option key={i} value={session.id}>{session.name}</option>
+                              ))}
+                            </Select>
+                          </div>
+                        )
+                      }
                     </div>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 md:flex md:flex-row-reverse md:px-6 md:gap-x-1.5 items-center">
-                  {importReady && (
-                    <PrimaryButton onClick={complete}>Import</PrimaryButton>
-                  )}
+                <div className="bg-gray-50 dark:bg-gray-800 px-4 py-3 md:flex md:flex-row-reverse md:px-6 md:gap-x-1.5 items-center">
+                  {importReady && <PrimaryButton onClick={complete}>Import</PrimaryButton>}
                   <SecondaryButton onClick={clickOut}>Cancel</SecondaryButton>
-                  {processing && (
-                    <div className="mr-auto italic">Processing Images...</div>
-                  )}
+                  {processing && <div className="mr-auto italic">Processing Images...</div>}
                   {!processing && importLaps?.length !== 0 && (
-                    <div className="mr-auto italic">
+                    <div className="mr-auto italic dark:text-gray-300">
                       {importLaps.length} laps detected
                     </div>
                   )}
